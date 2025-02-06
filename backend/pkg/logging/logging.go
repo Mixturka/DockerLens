ee1e53e68651daa1ed5f0da1f7a1faa70d1770d7
Mixturka <vadenko23@gmail.com>
2025-02-06 17:33:08 +0300

    ref: make logger shared module

--- backend/pkg/logging/logging.go
+++ backend/pkg/logging/logging.go
@@ -15,6 +15,8 @@ func SetupLogger(logLevel string) *slog.Logger {
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case "prod":
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	}
 
 	return log
